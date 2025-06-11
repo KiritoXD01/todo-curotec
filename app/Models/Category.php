8e4b8ceffdd1777081b2e6ce5b8952f834639d55
Javier Mercedes <manuelmercedez10@gmail.com>
2025-06-11 15:51:12 -0400

    Created component for categories

---
+++ app/Models/Category.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Category extends Model
+{
+    protected $fillable = [
+        'name',
+        'parent_id',
+        'user_id',
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(related: Category::class, foreignKey: 'parent_id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(related: User::class, foreignKey: 'user_id');
+    }
+
+    public function subcategories(): HasMany
+    {
+        return $this->hasMany(related: Category::class, foreignKey: 'parent_id');
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -46,5 +47,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function categories(): HasMany
+    {
+        return $this->hasMany(related: Category::class, foreignKey: 'user_id');
     }
 }
